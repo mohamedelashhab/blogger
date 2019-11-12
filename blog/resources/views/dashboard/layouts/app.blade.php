60ad65e84c0fe728f9035c960f018f9c498f8b61
ashhab <elashhab_fcih@yahoo.com>
2019-11-12 22:19:12 +0200

    dashboard simple design

---
+++ blog/resources/views/dashboard/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="{{url('dashboard/css/app.css')}}">
+</head>
+
+<!------ Include the above in your HEAD tag ---------->
+
+        
+        
+        @include('dashboard.layouts.nav')
+        @include('dashboard.layouts.sidebar')
+
+
+
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script> <!-- Menu Toggle Script -->
+        <script>
+          $(function(){
+            $("#menu-toggle").click(function(e) {
+                e.preventDefault();
+                $("#wrapper").toggleClass("toggled");
+            });
+
+            $(window).resize(function(e) {
+              if($(window).width()<=768){
+                $("#wrapper").removeClass("toggled");
+              }else{
+                $("#wrapper").addClass("toggled");
+              }
+            });
+          });
+           
+        </script>
+   
+  </html>
---
+++ blog/resources/views/dashboard/layouts/sidebar.blade.php
@@ -0,0 +1,20 @@
+        <div id="wrapper" class="toggled">
+            <!-- Sidebar -->
+            <div id="sidebar-wrapper">
+                <ul class="sidebar-nav">
+                    <li class="sidebar-brand"> <a href="#"> Blog </a> </li>
+                    <li> <a href="#">Dashboard</a> </li>
+                    <li> <a href="#">Users</a> </li>
+                    <li> <a href="#">Posts</a> </li>
+                    <li> <a href="#">Pages</a> </li>
+                    <li> <a href="#">Settings</a> </li>
+                    
+                </ul>
+            </div> <!-- /#sidebar-wrapper -->
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+                <div class="container-fluid">
+                    @yield('content')
+                </div>
+            </div> <!-- /#page-content-wrapper -->
+        </div> <!-- /#wrapper -->
